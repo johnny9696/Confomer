3a0e20e6ea4340e999bf08295b61a6dd4a996b79
johnny <johnny960423@gmail.com>
2022-10-25 15:25:47 +0900

    Model updated

--- Conformer.py
+++ Conformer.py
@@ -1,3 +1,5 @@
+from doctest import OutputChecker
+from tkinter import E
 import torch
 import torch.nn as nn
 import torch import Tensor
@@ -48,22 +50,41 @@ class Convolution_module(nn.Module):
 
 
 class Feed_forward_module(nn.Module):
-    def __init__(self,encoder_dim,expansion_factor,dropout_p):
+    def __init__(self,hidden_channels,expansion_factor,dropout_p):
         self.Layernorm=nn.LayerNorm
-        self.Linear=nn.Linear(encoder_dim,encoder_dim*expansion_factor)
+        self.Linear1=nn.Linear(hidden_channels,hidden_channels*expansion_factor)
         self.swish=Swish()
+        self.Linear2=nn.Linear(hidden_channels*expansion_factor,hidden_channels)
         self.dropout=nn.Dropout(p=dropout_p)
     def forward(self,x):
-        x=self.Layernorm(x)
-        x=self.Linear(x)
+        """
+        x : [batch, h, hidden_channels]
+        """
+        x=self.Layernorm(x) #[batch, h, hidden_channels]
+        x=self.Linear1(x) # [batch, h, hidden_channels*Expansion_factor]
         x=self.swish(x)
         x=self.dropout(x)
-        x=self.Linear(x)
+        x=self.Linear2(x) # [batch, h, hidden_channels]
         x=self.dropout(x)
         return x
 
+class Position_encoding(nn.Module):
+    def __init__(self,d_model=512,max_len=10000):
+        """
+        even  position : sin(pos/power(10000,2i/d_model))
+        odd position : cos(pos/power(100000,2i/d_model))
+        """
+
+    def forward(self):
+
 class  MHSA(nn.Module):
-    def __init__(self):
+    def __init__(self,
+    dropout_p):
+        self.Layer_norm=nn.LayerNorm()
+        self.dropout=nn.Dropout(p=dropout_p)
+        self.position_encoding=Position_encoding()
+    def forward(self,x):
+        return x
 
 class convolution_subsampling(nn.Module):
     def __init__(self,
@@ -76,32 +97,48 @@ class convolution_subsampling(nn.Module):
             nn.ReLU()
         )
     def forward(self,x):
-        output=self.sequential(x.unsqueeze(1))
-
-
-
+        """
+        Input : Mel Spectrogram Shape [batch, n_Mels, Frames]
+        After Unsqueeze(1) [batch, 1 , n_Mels , Frames]
+        After SubSampling [batch, out_channels , t , h ]
+        To Use Linear Function Need to change to 2D
+        [batch, out_channels*t,h] -> Transpose [batch,h,out_channels*t]
+        t=((((n_mels-3)//2+1)-3)//2)+1
+        """
+        output = self.sequential(x.unsqueeze(1))
+        batch,channels,length,height=output.size()
+        output=output.contigous().view(batch,channels*length,height)
+        output=output.permute(0,2,1)
+        return output
 
 class Conformer_block(nn.Module):
-    def __init__(self):
-        self.feed_forward=Feed_forward_module()
-        self.MHSA=MHSA()
-        self.convolution=Convolution_module()
-        self.Layernorm=nn.Layernorm()
+    def __init__(self,
+    dropout_p,
+    expansion_factor,
+    hidden_channels):
+        self.feed_forward = Feed_forward_module(hidden_channels=hidden_channels,expansion_factor=expansion_factor,dropout_p=dropout_p)
+        self.MHSA = MHSA()
+        self.convolution = Convolution_module()
+        self.Layernorm = nn.Layernorm()
 
     def residual_connect(self,x,orig_x):
-        x=x+orig_x
+        x = x+orig_x
         return x,x
     
     def forward(self,x):
-        orig_x=x
-        x=self.feed_forward(x)
-        orgi_x,x=self.residual_connect(x,orgi_x)
+        """
+        Block Input Shape :  [batch, h, hidden_channels]
+        After Feed forward : [batch, h, hidden_channels]
+        """
+        orig_x = x
+        x = self.feed_forward(x)
+        orgi_x, x = self.residual_connect(torch.div(x,0.5),orgi_x)
         x=self.MHSA(x)
         orgi_x,x=self.residual_connect(x,orgi_x)
-        x=self.convolution(x)
+        x=self.convolution(x) 
         orgi,x=self.residual_connect(x,orgi_x)
         x=self.feed_forward(x)
-        orgi_x,x=self.residual_connect(x,orgi_x)
+        orgi_x,x=self.residual_connect(torch.div(x,0.5),orgi_x)
         x=self.Layernorm(x)
         return x
 
@@ -109,21 +146,22 @@ class Conformer(nn.Module):
     def __init__(self,
     dropout_p=0.4,
     n_Conf_block=6,
+    n_mels,
     n_class,
     hidden_channel,
     input_channel,):
-        self.Conv_sub=convolution_subsampling()
-        self.Linear=nn.Linear()
-        self.dropout=nn.dropout(p=dropout_p)
-        self.block=Conformer_block()
-        conformer_block=[]
+        self.Conv_sub = convolution_subsampling(in_channels=1,out_channels=hidden_channel) #[b,h,out_channels*t]
+        self.Linear = nn.Linear(hidden_channel*((((n_mels-3)//2+1)-3)//2+1), hidden_channel) #[batch, h, hidden_channels]
+        self.dropout = nn.dropout(p=dropout_p) # [batch, h, hidden_channels]
+        self.block = Conformer_block()
+        conformer_block = []
         for i in n_Conf_block:
             conformer_block.append(self.block)
-        self.conformer_block=nn.Sequencial(*conformer_block)
-        self.FClayer=nn.Linear(,n_class)
+        self.conformer_block = nn.Sequencial(*conformer_block)
+        self.FClayer = nn.Linear(,n_class)
     
     def forward(self,x):
-        #input x [b,]
+        #input x [b,n_mels,frames]
         x=self.Conv_sub(x)
         x=self.Linear(x)
         x=self.dropout(x)
